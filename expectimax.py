e41b7e7d670e3719ebd74b930f2e5617aa345184
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2020-03-24 17:52:33 +0100

    Update expectimax.py
    Greatly improve expectimax!
    Now checks if the move is possible at all :ok_hand:

--- expectimax.py
+++ expectimax.py
@@ -6,10 +6,10 @@ def expectimax(board: list, depth: int):
     if depth == 0 or check_game(board) == -1:
         return evaluate(board)
     elif depth % 2 == 0:
-        return max(expectimax(left(board), depth-1), 
-                   expectimax(right(board), depth-1), 
-                   expectimax(down(board), depth-1),
-                   expectimax(up(board), depth-1))
+        return max(expectimax(left(board), depth-1) if left(board) != board else -1001, 
+                   expectimax(right(board), depth-1) if right(board) != board else -1001, 
+                   expectimax(down(board), depth-1) if down(board) != board else -1001,
+                   expectimax(up(board), depth-1) if up(board) != board else -1001)
     else:
         sum2, sum4 = (0, 0)
         twos, fours = get_possible_states(board)
