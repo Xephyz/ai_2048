c195beaa623c212ae51c83fa7b24dc62463d2fde
xephyz <xephyzone@gmail.com>
2020-03-24 16:26:26 +0100

    Smol fixes
    Co-Authored-By: nicklasdancker <nicklasdancker@users.noreply.github.com>
    Co-Authored-By: Lysberg <lysberg@users.noreply.github.com>

--- expectimax.py
+++ expectimax.py
@@ -2,17 +2,17 @@
 from logic import check_game, get_possible_states, up, down, left, right
 from heuristics import evaluate
 
-def expectimax(board: list, depth: int = 2):
+def expectimax(board: list, depth: int):
     if depth == 0 or check_game(board) == -1:
         return evaluate(board)
-    elif d % 2 != 0:
+    elif depth % 2 == 0:
         return max(expectimax(left(board), depth-1), 
                    expectimax(right(board), depth-1), 
-                   expectimax(down(), depth-1),
+                   expectimax(down(board), depth-1),
                    expectimax(up(board), depth-1))
     else:
         sum2, sum4 = (0, 0)
         twos, fours = get_possible_states(board)
-        for state2 in twos: sum2 += expectimax(state2)
-        for state4 in fours: sum4 += expectimax(state4)
+        for state2 in twos: sum2 += expectimax(state2, depth-1)
+        for state4 in fours: sum4 += expectimax(state4, depth-1)
         return (0.9 * sum2 + 0.1 * sum4)/len(twos)
--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from logic import new_matrix, add_random_tile, left, right, down, up, print_boar
 class Game:
     """The actual 2048 game class"""
     def __init__(self, initial_board: list = new_matrix(4)):
-        self.board = initial_board
+        self.board = add_random_tile(add_random_tile(initial_board))
 
     def move(self, move: str) -> None:
         if move == 'l': self.board = add_random_tile(left(self.board))
