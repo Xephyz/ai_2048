be496c342e82ea8f1a769fefe75e109f28bf11e4
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2020-03-24 20:34:03 +0100

    No deepcopy in game
    deepcopy removed from game :ok_hand:

--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 # The actual 2048 game!
 # Why have we only done this now?
 from logic import new_matrix, add_random_tile, left, right, down, up, print_board
-from copy import deepcopy
 
 class Game:
     """The actual 2048 game class"""
@@ -9,16 +8,15 @@ class Game:
         self.board = add_random_tile(add_random_tile(initial_board))
 
     def move(self, move: str) -> None:
-        old = deepcopy(self.board)
         if move == 'l':
             print('Going left')
-            self.board = add_random_tile(left(self.board)) if left(self.board) != old else self.board
+            self.board = add_random_tile(left(self.board)) if left(self.board) != self.board else self.board
         elif move == 'r':
             print('Going right')
-            self.board = add_random_tile(right(self.board)) if right(self.board) != old else self.board
+            self.board = add_random_tile(right(self.board)) if right(self.board) != self.board else self.board
         elif move == 'd':
             print('Going down')
-            self.board = add_random_tile(down(self.board)) if down(self.board) != old else self.board
+            self.board = add_random_tile(down(self.board)) if down(self.board) != self.board else self.board
         elif move == 'u':
             print('Going up')
-            self.board = add_random_tile(up(self.board)) if up(self.board) != old else self.board
+            self.board = add_random_tile(up(self.board)) if up(self.board) != self.board else self.board
