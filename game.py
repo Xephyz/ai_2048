723323c4171d63efd7877aa7a8b3c9f1e12ff34d
xephyz <xephyzone@gmail.com>
2020-03-24 17:13:31 +0100

    Update game.py
    Game checks for stoopid moves :ok_hand:
    
    Co-Authored-By: nicklasdancker <nicklasdancker@users.noreply.github.com>

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # The actual 2048 game!
 # Why have we only done this now?
 from logic import new_matrix, add_random_tile, left, right, down, up, print_board
+from copy import deepcopy
 
 class Game:
     """The actual 2048 game class"""
@@ -8,8 +9,16 @@ class Game:
         self.board = add_random_tile(add_random_tile(initial_board))
 
     def move(self, move: str) -> None:
-        if move == 'l': self.board = add_random_tile(left(self.board))
-        if move == 'r': self.board = add_random_tile(right(self.board))
-        if move == 'd': self.board = add_random_tile(down(self.board))
-        if move == 'u': self.board = add_random_tile(up(self.board))
-        print_board(self.board)
+        old = deepcopy(self.board)
+        if move == 'l':
+            print('Going left')
+            self.board = add_random_tile(left(self.board)) if left(self.board) != old else self.board
+        elif move == 'r':
+            print('Going right')
+            self.board = add_random_tile(right(self.board)) if right(self.board) != old else self.board
+        elif move == 'd':
+            print('Going down')
+            self.board = add_random_tile(down(self.board)) if down(self.board) != old else self.board
+        elif move == 'u':
+            print('Going up')
+            self.board = add_random_tile(up(self.board)) if up(self.board) != old else self.board
