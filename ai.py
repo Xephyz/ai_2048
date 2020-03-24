8e0f10bd9504ff6466371b4a4b04e4dd387a6148
xephyz <xephyzone@gmail.com>
2020-03-24 17:14:57 +0100

    Initial ai working! :droplet: :ok_hand:
    DUUUUUUIII!!!!
    
    Co-Authored-By: nicklasdancker <nicklasdancker@users.noreply.github.com>

---
+++ ai.py
@@ -0,0 +1,39 @@
+# Ai does gaem veri gut yes
+
+from game import Game
+from logic import left, right, down, up, print_board as pb
+from expectimax import expectimax as exp
+from copy import deepcopy
+from sys import platform
+from os import system
+from time import sleep
+
+if __name__ == "__main__":
+    clear_cmd = 'cls' if platform == 'win32' else 'clear'
+
+    g = Game()
+    d = 3  # default depth
+    g.board = [[0,0,0,0],[0,0,2,0],[2,0,0,0],[0,0,0,0]]
+
+    while True:
+        old = deepcopy(g.board)
+        # system(clear_cmd)
+        # pb(g.board)
+
+        pos_moves = []
+        pos_moves.append(exp(left(g.board), d) if left(g.board) != old else -1001)
+        pos_moves.append(exp(right(g.board), d) if right(g.board) != old else -1001)
+        pos_moves.append(exp(down(g.board), d) if down(g.board) != old else -1001)
+        pos_moves.append(exp(up(g.board), d) if up(g.board) != old else -1001)
+
+        best_move = pos_moves.index(max(pos_moves))
+
+        if best_move == 0: g.move('l')
+        elif best_move == 1: g.move('r')
+        elif best_move == 2: g.move('d')
+        else: g.move('u')
+        system(clear_cmd)
+        print(pos_moves)
+        pb(g.board)
+
+        # sleep(0.5)
