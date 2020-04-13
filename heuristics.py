33e13625e4e175a798cd3e2defb41b07883d16a3
xephyz <xephyzone@gmail.com>
2020-04-13 19:37:19 +0200

    Update heuristics.py
    Refactor, aka. be slightly more PEP8-y :ok_hand:

--- heuristics.py
+++ heuristics.py
@@ -1,9 +1,9 @@
 from logic import transpose as tr, reverse as rev, check_game as cg
 
-pattern = [[16,  9,   8,  1],
-          [15,  10,  7,  2],
-          [14,  11,  6,  3],
-          [13,  12,  5,  4]]
+pattern = [[16,   9,  8,  1],
+           [15,  10,  7,  2],
+           [14,  11,  6,  3],
+           [13,  12,  5,  4]]
 
 
 def _build_patterns(p1: list = pattern):
@@ -16,21 +16,23 @@ def _build_patterns(p1: list = pattern):
     p8 = tr(p7)
     return [p1, p2, p3, p4, p5, p6, p7, p8]
 
+
 patterns = _build_patterns()
 
+
 def _get_state_value(state: list):
-    sums = [0]*len(patterns)
+    sums = [0] * 8  # 8 represents 'len(patterns)', which is always 8
 
     for i in range(len(state)):
         for j in range(len(state)):
-            for k in range(len(patterns)):
+            for k in range(8):
                 sums[k] += patterns[k][i][j] * state[i][j]
 
     return max(sums)
+
 
 def evaluate(state: list):
     if cg(state) == -1: return -1000
     evalue = _get_state_value(state)
     if cg(state) == 1: return evalue + 1000
     else: return evalue
-
